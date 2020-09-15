f90be15413f68e3b94406d00a5495b8509176d52
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2020-09-15 03:47:31 +0300

    Remove the IDE trash from the repository

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,18 @@ if [ -z "${SERVICE:-}" ]; then
   exit 1
 fi
 
-if [ -z "${TAG:-}" ]; then
-  TAG="$(git describe --dirty)"
-  echo "🎈 Using ${TAG}!" >&2
-fi
+#if [ -z "${TAG:-}" ]; then
+#  TAG="$(git describe --dirty)"
+#  echo "🎈 Using ${TAG}!" >&2
+#fi
 
-docker build -t simple-tgbot --build-arg SERVICE=tgbot .
+TAG=$(git config --get remote.origin.url)
+GITHUB_SSH_PREFIX="git@github.com:"
+GITHUB_HTTPS_PREFIX="https://github.com"
+GIT_SUFFIX=".git"
+
+TAG=${TAG#$GITHUB_SSH_PREFIX}
+TAG=${TAG#$GITHUB_HTTPS_PREFIX}
+TAG=${TAG%$GIT_SUFFIX}
+
+docker build -t "docker.pkg.github.com/${TAG}/${SERVICE}:latest" --build-arg SERVICE=tgbot .
